443087cdd52e24ac283f4a1150dd5a374823df77
Fred Dubois <169247+duboisf@users.noreply.github.com>
2020-06-15 08:22:34 -0400

    Tweak zsh syntax highlighting

---
+++ zsh/.zsh/lib/syntax-highlighting.zsh
@@ -0,0 +1,9 @@
+# See
+# https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters/main.md
+# for documentation
+
+# To differentiate aliases from other command types
+ZSH_HIGHLIGHT_STYLES[alias]='fg=magenta'
+
+# To have paths colored instead of underlined
+ZSH_HIGHLIGHT_STYLES[path]='fg=cyan'
