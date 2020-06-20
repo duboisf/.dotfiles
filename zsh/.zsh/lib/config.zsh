3ba280f3212b5d215c26f16cdb73c6e63d860546
Fred Dubois <169247+duboisf@users.noreply.github.com>
2020-06-20 17:17:43 -0400

    Use fd for fzf zle widgets

--- zsh/.zsh/lib/config.zsh
+++ zsh/.zsh/lib/config.zsh
@@ -5,7 +5,7 @@ zstyle ':completion:*' special-dirs false
 
 # Directory navigation lifehacks
 ################################
-#
+
 # Use Esc-l (or Alt-l) to list directory content
 bindkey -s '\el' 'l\n'
 # Use Esc+u (or Alt+u) to go up to the parent folder
@@ -19,3 +19,15 @@ zstyle ':chpwd:*' recent-dirs-max 1000
 # Bind [Esc-j] (or Alt-j) to fuzzy find a semi-recently visited folder
 bindkey -s '\ej' "cdr \$(cdr -l | egrep -v ' ~$' | fzf --with-nth=2 | awk '{print \$1}')\n"
 
+# fzf config
+############
+if (( $+commands[fzf] )); then
+    FD_BIN_PATH=$commands[fd]
+    [[ -z $FD_BIN_PATH ]] && FD_BIN_PATH=$commands[fdfind]
+    if [[ -n $FD_BIN_PATH ]]; then
+        # By default fzf uses find, let's use fd instead
+        FZF_CTRL_T_COMMAND="$FD_BIN_PATH --type f --hidden --no-ignore-vcs"
+        FZF_ALT_C_COMMAND="$FD_BIN_PATH --type d --hidden --no-ignore-vcs"
+    fi
+    unset FD_BIN_PATH
+fi
