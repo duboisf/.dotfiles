d1cdb28fd0a398bd709fae783711b13b4e5bba49
Fred Dubois <169247+duboisf@users.noreply.github.com>
2020-11-25 08:05:30 -0500

    Remove set -x

--- zsh/.zsh/lib/completion.zsh
+++ zsh/.zsh/lib/completion.zsh
@@ -1,10 +1,8 @@
 
 if (( $+commands[kubectl] )); then
-    set -x
     local _kubectl_path=$(dirname $0)/../completions/_kubectl
     if [[ ! -f _kubectl_path ]]; then
         kubectl completion zsh > $_kubectl_path
     fi
     unset _kubectl_path
-    set +x
 fi
