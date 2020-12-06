ff913e330f8402b63b0416b4cffb294deedde75d
Fred Dubois <169247+duboisf@users.noreply.github.com>
2020-12-06 09:51:07 -0500

    zsh: add DOCKER_BUILDKIT env var

--- zsh/.zsh/lib/env.zsh
+++ zsh/.zsh/lib/env.zsh
@@ -11,8 +11,9 @@ if [[ -d ~/.tfenv/bin ]]; then
     path=(~/.tfenv/bin $path)
 fi
 
-export MANPAGER='nvim --cmd "let g:pager_mode = 1" +Man!'
-export MANWIDTH=999
+export DOCKER_BUILDKIT=1
 export EDITOR=nvim
 export GPG_TTY=$(tty)
+export MANPAGER='nvim --cmd "let g:pager_mode = 1" +Man!'
+export MANWIDTH=999
 export NVM_DIR=~/.nvm
