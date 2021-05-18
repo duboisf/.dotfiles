7053ccfdda5546c3435feb13567d7bb622610778
Fred Dubois <169247+duboisf@users.noreply.github.com>
2021-05-18 16:09:56 -0400

    zsh: source brew env vars if installed

--- zsh/.zsh/lib/env.zsh
+++ zsh/.zsh/lib/env.zsh
@@ -17,3 +17,14 @@ export GPG_TTY=$(tty)
 export MANPAGER='nvim --cmd "let g:pager_mode = 1" +Man!'
 export MANWIDTH=999
 export NVM_DIR=~/.nvm
+
+if [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    if [[ -f ~/.config/gh/hosts.yml ]]; then
+        export HOMEBREW_GITHUB_API_TOKEN=$(perl -wlne '/oauth_token: (.*)/ and print $1' ~/.config/gh/hosts.yml)
+    else
+        echo "brew is installed but we can't define HOMEBREW_GITHUB_API_TOKEN since gh isn't installed, or auth hasn't been setup yet (~/.config/gh/hosts.yml)" >&2
+    fi
+fi
+
+
