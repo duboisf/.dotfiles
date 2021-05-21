f3bda2cf2f12fb2a9875a586cede57058147ece5
Fred Dubois <169247+duboisf@users.noreply.github.com>
2021-05-21 16:40:56 -0400

    zsh: define aliases unconditionally

--- zsh/.zsh/lib/aliases.zsh
+++ zsh/.zsh/lib/aliases.zsh
@@ -33,124 +33,96 @@ alias mk='minikube'
 alias kns=kubens
 alias kctx=kubectx
 
-# codefresh
-###########
-if (( $+commands[codefresh] )); then
-    alias cf=codefresh
-fi
-
 # kubectl
 #########
-if (( $+commands[kubectl] )); then
-    alias kc='kubectl'
-    alias skc='kubectl --as=$USER --as-group=system:masters'
-    alias rpo='kc get po --field-selector=status.phase=Running'
-    alias po='kc get po'
-    alias dpo='kc describe po'
-    alias deploy='kc get deploy'
-    alias ddeploy='kc describe deploy'
-    alias logs='kc logs'
-    alias no='kc --as=$USER --as-group=system:masters get no'
-    alias dno='kc --as=$USER --as-group=system:masters describe no'
-    alias svc='kc get svc'
-    alias dsvc='kc describe svc'
-    alias rs='kc rollout status'
-    alias ds='kc get ds'
-    alias dds='kc describe ds'
-    alias configmap='kc get configmap'
-    alias dconfigmap='kc describe configmap'
-    alias cm='kc get configmap'
-    alias dcm='kc describe configmap'
-    alias secret='kc get secret'
-    alias dsecret='kc describe secret'
-    if (( $+commands[jq] )); then
-        # base64 decode kubebernetes secrets
-        alias -g DS="-o yaml | yq --yaml-roundtrip '. + {"stringData": (.data | map_values(@base64d))} | del(.data)'"
-    fi
-    alias jo='kc get job'
-    alias djo='kc describe job'
-    alias cronjob='kc get cronjob'
-    alias dcronjob='kc describe cronjob'
-    alias rollouts='kc rollout status'
-    alias -g CT='--context'
-    alias -g AN='--all-namespaces'
-    alias -g J='-o json'
-    alias -g Y='-o yaml'
-    alias kubelistall='kubectl api-resources --verbs=list --namespaced -o name | grep -v event | xargs -n 1 kubectl get --show-kind --ignore-not-found'
-fi
+alias kc='kubectl'
+alias skc='kubectl --as=$USER --as-group=system:masters'
+alias rpo='kc get po --field-selector=status.phase=Running'
+alias po='kc get po'
+alias dpo='kc describe po'
+alias deploy='kc get deploy'
+alias ddeploy='kc describe deploy'
+alias logs='kc logs'
+alias no='kc --as=$USER --as-group=system:masters get no'
+alias dno='kc --as=$USER --as-group=system:masters describe no'
+alias svc='kc get svc'
+alias dsvc='kc describe svc'
+alias rs='kc rollout status'
+alias ds='kc get ds'
+alias dds='kc describe ds'
+alias configmap='kc get configmap'
+alias dconfigmap='kc describe configmap'
+alias cm='kc get configmap'
+alias dcm='kc describe configmap'
+alias secret='kc get secret'
+alias dsecret='kc describe secret'
+# base64 decode kubebernetes secrets
+alias -g DS="-o yaml | yq --yaml-roundtrip '. + {"stringData": (.data | map_values(@base64d))} | del(.data)'"
+alias jo='kc get job'
+alias djo='kc describe job'
+alias cronjob='kc get cronjob'
+alias dcronjob='kc describe cronjob'
+alias rollouts='kc rollout status'
+alias -g CT='--context'
+alias -g AN='--all-namespaces'
+alias -g J='-o json'
+alias -g Y='-o yaml'
+alias kubelistall='kubectl api-resources --verbs=list --namespaced -o name | grep -v event | xargs -n 1 kubectl get --show-kind --ignore-not-found'
 
 # git
 #####
-if (( $+commands[git] )); then
-    alias g='git'
-    alias gb='git branch'
-    alias gco='git checkout'
-    alias ga='git add'
-    alias gs='git status --short'
-    alias gl='git log --decorate'
-    alias gl1p='git log --decorate -1 -p'
-    alias gls='git log --decorate --stat'
-    alias gci='git commit -v'
-    alias gpl='git pull'
-    alias cdg='cd "$(git rev-parse --show-toplevel)"'
-    alias gciah='git commit --amend -C HEAD'
-    alias gr='git rebase'
-    alias grc='git rebase --continue'
-    alias gra='git rebase --abort'
-    alias gd='git diff'
-    alias gdc='git diff --cached'
-fi
+alias g='git'
+alias gb='git branch'
+alias gco='git checkout'
+alias ga='git add'
+alias gs='git status --short'
+alias gl='git log --decorate'
+alias gl1p='git log --decorate -1 -p'
+alias gls='git log --decorate --stat'
+alias gci='git commit -v'
+alias gpl='git pull'
+alias cdg='cd "$(git rev-parse --show-toplevel)"'
+alias gciah='git commit --amend -C HEAD'
+alias gr='git rebase'
+alias grc='git rebase --continue'
+alias gra='git rebase --abort'
+alias gd='git diff'
+alias gdc='git diff --cached'
 
 # github gh
 ###########
-if (( $+commands[gh] )); then
-    alias p='gh pr'
-    alias prv='p view'
-    alias prc='p create'
-    alias prco='p checkout'
-    alias prlv='p list -w'
-fi
+alias p='gh pr'
+alias prv='p view'
+alias prc='p create'
+alias prco='p checkout'
+alias prlv='p list -w'
 
 # terraform
 ###########
-if (( $+commands[terraform] )); then
-    alias tf=terraform
-    alias tfi='tf init'
-    alias tfp='tf plan -out=plan.out'
-    alias tfpc='tfp | perl -wlne "/^(..0m)?(\s{6}\S)/ or /^..0m$/ or print"'
-    alias tfs='tf show plan.out'
-    alias tfsc='tfs | perl -wlne "/^(..0m)?(\s{6}\S)/ or /^(..0m){1,2}$/ or print"'
-    alias tfv='tf validate'
-    alias tfsl='tf state list'
-    alias tfss='tf state show'
-    alias tfw='tf workspace'
-    alias tfwl='tfw list'
-    alias tfws='tfw select'
-    alias tfwss='tfw show'
-fi
+alias tf=terraform
+alias tfi='tf init'
+alias tfp='tf plan -out=plan.out'
+alias tfpc='tfp | perl -wlne "/^(..0m)?(\s{6}\S)/ or /^..0m$/ or print"'
+alias tfs='tf show plan.out'
+alias tfsc='tfs | perl -wlne "/^(..0m)?(\s{6}\S)/ or /^(..0m){1,2}$/ or print"'
+alias tfv='tf validate'
+alias tfsl='tf state list'
+alias tfss='tf state show'
+alias tfw='tf workspace'
+alias tfwl='tfw list'
+alias tfws='tfw select'
+alias tfwss='tfw show'
 
-# Misc
-alias setup_monitors='xrandr --output DP-1-2-2 --mode 2560x1440 --rotate right --output HDMI-1-1 --mode 2560x1440 --right-of DP-1-2-2 --output eDP-1-1 --mode 2560x1440 --right-of HDMI-1-1'
+# golang
+########
 alias rdlv='dlv --listen=:2345 --headless=true --api-version=2 --accept-multiclient'
 
-# tmux
-######
-if (( $+commands[tmux] )); then
-    alias tn='tmux new -s'
-    alias ta='tmux attach -t'
-fi
-
-if (( $+commands[fdfind] )); then
-    alias fd=fdfind
-fi
-
-
-if (( $+commands[helm] )); then
-    alias h=helm
-    alias hl='h ls'
-    alias hu='h repo up'
-    alias hsh='h search hub'
-    alias hs='h search repo'
-    alias hsd='h search repo --devel'
-    alias hru='h repo up'
-fi
+# helm
+#######
+alias h=helm
+alias hl='h ls'
+alias hu='h repo up'
+alias hsh='h search hub'
+alias hs='h search repo'
+alias hsd='h search repo --devel'
+alias hru='h repo up'
