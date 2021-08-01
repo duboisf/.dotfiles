df1d9da4208d804826299623e2dcc4bd8473b358
Fred Dubois <169247+duboisf@users.noreply.github.com>
2021-08-01 12:55:46 -0400

    🔧 nvim: add tree-sitter incremental selection

--- nvim/.config/nvim/lua/duboisf/config/treesitter.lua
+++ nvim/.config/nvim/lua/duboisf/config/treesitter.lua
@@ -9,10 +9,10 @@ local treesitterConfig = {
     incremental_selection = {
         enable = true,
         keymaps = {                       -- mappings for incremental selection (visual mappings)
-          -- init_selection    = 'gs',       -- maps in normal mode to init the node/scope selection
-          -- node_incremental  = 'gj',       -- increment to the upper named parent
-          -- scope_incremental = 'gl',       -- increment to the upper scope (as defined in locals.scm)
-          -- node_decremental  = 'gk',       -- decrement to the previous node
+          init_selection    = 'gs',       -- maps in normal mode to init the node/scope selection
+          node_incremental  = 'gj',       -- increment to the upper named parent
+          scope_incremental = 'gl',       -- increment to the upper scope (as defined in locals.scm)
+          node_decremental  = 'gk',       -- decrement to the previous node
         }
     },
     -- indent = {
