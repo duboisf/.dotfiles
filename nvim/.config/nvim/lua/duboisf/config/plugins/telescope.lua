1d42452b61d6fd36775c4fb005106b01002949a1
Fred Dubois <169247+duboisf@users.noreply.github.com>
2022-06-16 14:31:23 -0400

    ✨ nvim: use fzf extension for telescope

--- nvim/.config/nvim/lua/duboisf/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/duboisf/config/plugins/telescope.lua
@@ -72,7 +72,8 @@ require('telescope').setup {
   }
 }
 
-require('telescope').load_extension 'file_browser'
+require'telescope'.load_extension 'file_browser'
+require'telescope'.load_extension 'fzf'
 
 local M = {}
 
