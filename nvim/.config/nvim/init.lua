2824e3c25af4d5a119e0cf753e2cb78e9f3d7e95
Fred Dubois <169247+duboisf@users.noreply.github.com>
2022-06-18 09:47:01 -0400

    🔧 nvim: moved everything to init.lua 🎉

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,19 @@
 local fn = vim.fn
 
+-- define a bunch of autocommands to help with lazy loading
+local group = vim.api.nvim_create_augroup('duboisf', { clear = true })
+
+local autocmd = function(events, callback)
+  if type(events) == 'string' then
+    events = { events }
+  end
+  vim.api.nvim_create_autocmd(events, {
+    group = group,
+    pattern = "*",
+    callback = callback
+  })
+end
+
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 local packer_bootstrap = false
 if fn.empty(fn.glob(install_path)) > 0 then
@@ -7,7 +21,6 @@ if fn.empty(fn.glob(install_path)) > 0 then
     install_path })
 end
 
-local group = vim.api.nvim_create_augroup("duboisf#config#plugins", {})
 vim.api.nvim_create_autocmd("BufWritePost", {
   group = group,
   desc = "Run packer.compile() when any plugin lua config file gets saved",
@@ -255,3 +268,164 @@ require('packer').startup({function(use)
     require('packer').sync()
   end
 end, config = { profile = { enable = true, threshold = 1 } } })
+
+vim.cmd [[
+" Check if we started nvim in pager mode
+let g:pager_mode = get(g:, 'pager_mode', v:false)
+
+"""""""""""""""""""""
+" vim configuration "
+"""""""""""""""""""""
+set termguicolors " Enables 24-bit RGB color in the Terminal UI
+set mouse=a
+set relativenumber
+set number
+set textwidth=0
+set tabstop=4
+set softtabstop=4
+set shiftwidth=4
+set expandtab
+set encoding=utf-8
+set listchars=tab:→\ ,trail:·,eol:↩
+set list
+set scrolloff=0
+set cursorline
+if !&diff
+  set cursorcolumn
+endif
+set wildignore+=*/.git/*,*/.hg/*,*/.svn/*
+set dictionary+=/usr/share/dict/american-english
+" Remove = from isfname so that completing a filename with <C-x><C-f>
+" works even in an assignment like SOME_VAR=/foo/bar. The side effect of this
+" will be that gf won't work with filenames with = in them... I don't think
+" this will cause a lot of problems...
+set isfname-==
+" if hidden is not set, TextEdit might fail.
+set hidden
+" Don't show the current mode (insert, visual, etc.)
+set noshowmode
+" Better display for messages
+set cmdheight=2
+" Smaller updatetime for CursorHold & CursorHoldI
+set updatetime=100
+" don't give |ins-completion-menu| messages.
+set shortmess+=c
+
+aug fred#signcolumn
+  au!
+  " always show signcolumns. We set it on BufRead, otherwise on nvim startup 
+  " we see the startup screen flash for a moment and disappear. This is
+  " because setting signcolumn hides the startup screen message.
+  au BufRead * setlocal signcolumn=yes:2
+aug end
+
+" set signcolumn=yes:2
+" show interactive substitute
+set inccommand=nosplit
+
+" tweak settings if we are using nvim as a pager, we know this because
+" we set the pager_mode variable when we do
+if g:pager_mode
+  set nolist
+  set nowrap
+  " use q in normal mode to quit, like in less
+  nnoremap q :q<CR>
+endif
+
+" Use the spacebar as the key for <leader> mappings
+let mapleader=' '
+
+" Miscellaneous mappings
+nnoremap <leader>q :q<CR>
+nnoremap <leader>s :w<CR>
+nnoremap <leader>b :bd<CR>
+nnoremap <leader>m <C-W>_
+nnoremap <leader>= <C-W>=
+nnoremap <leader>. 10<C-W>>
+nnoremap <leader>, 10<C-W><
+" Move selection up or down
+xnoremap <C-Up> xkP`[V`]
+xnoremap <C-Down> xp`[V`]
+" Toggle wrap
+nnoremap <leader>w :set wrap!<CR>
+" Toggle paste
+nnoremap <leader>p :set paste!<CR>
+" Yank highlighted selection in visual mode to clipboard
+vnoremap <leader>y "+y
+" Yank highlighted lines in visual mode to clipboard
+vnoremap <leader>Y "+Y
+" Tab and Shift-Tab keys cycle through buffers
+nnoremap <Tab> :bn<CR>
+nnoremap <S-Tab> :bp<CR>
+
+" jump to last position when reopening a file
+aug fred#jump_to_last_position
+  au!
+  au Filetype gitcommit let b:disable_jump_to_last_position = 1
+  au BufReadPost * call s:JumpToLastPositionWhenReopeningBuffer()
+  fun s:JumpToLastPositionWhenReopeningBuffer()
+    if exists('b:disable_jump_to_last_position')
+      return
+    endif
+    if line("'\"") > 0 && line("'\"") <= line("$")
+      exe "normal! g`\""
+    endif
+  endfun
+aug end
+
+" tweak man mode
+aug fred#man
+  au!
+  au Filetype man call s:TweakManSettings()
+  fu s:TweakManSettings()
+    echo "TweakManSettings"
+    set nocursorline
+    set nocursorcolumn
+    nnoremap f <C-f>
+    nnoremap d <C-b>
+  endfu
+aug end
+
+"########################
+"# PLUGIN CONFIGURATION #
+"########################
+
+" editorconfig configuration
+let g:EditorConfig_exclude_patterns = ['fugitive://.*']
+
+" startify configuration
+" don't change cwd when jumping to a file
+let g:startify_change_to_dir = 0
+
+let g:startify_lists = [
+      \ { 'type': 'dir',       'header': ['   MRU '. getcwd()] },
+      \ { 'type': 'files',     'header': ['   MRU']            },
+      \ ]
+
+" firenvim configuration
+let g:firenvim_config = {
+    \ 'globalSettings': {
+        \ 'alt': 'all',
+    \  },
+    \ 'localSettings': {
+        \ '.*': {
+            \ 'cmdline': 'firenvim',
+            \ 'priority': 0,
+            \ 'selector': 'textarea',
+            \ 'takeover': 'never',
+        \ },
+    \ }
+\ }
+
+"""""""""""""""""""""
+" Misc autocommands "
+"""""""""""""""""""""
+
+let g:loaded_netrw       = 1
+let g:loaded_netrwPlugin = 1
+
+au TextYankPost * lua vim.highlight.on_yank { higroup="IncSearch", timeout=300, on_visual=true }
+
+]]
+
+-- vim: sts=2 sw=2 ts=2 et
