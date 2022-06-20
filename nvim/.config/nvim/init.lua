5c63879a12789aa5a50d991851e6302b9fad6195
Fred Dubois <169247+duboisf@users.noreply.github.com>
2022-06-20 11:35:57 -0400

    🔧 nvim: fix jumping to last position when opening a file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -20,27 +20,37 @@ end
 
 -- Jump to last position when reopening a buffer
 local group = autogroup('cfg#jump_to_last_position', true)
-autocmd(group, 'Filetype', "*", function() vim.b.disable_jump_to_last_position = 1 end)
-autocmd(group, 'BufReadPost', "*", function()
-  if vim.b.disable_jump_to_last_position then
-    return
-  end
-  if fn.line("'\"") > 0 and fn.line("'\"") <= fn.line("$") then
-    fn.execute('normal! g`\"')
-  end
-end
-  , {
-  desc = 'Jump to last position when reopening a buffer'
-})
-
-vim.keymap.set('n', '<C-o>', function()
-  local ts_utils = require'nvim-treesitter.ts_utils'
-  local node = ts_utils.get_node_at_cursor(0, true)
-  if not node then
-    return
-  end
-  print(node:type())
+-- disable jumping to last position when writting git commit messages
+autocmd(group, 'FileType', "gitcommit", function()
+  vim.b.disable_jump_to_last_position = true
 end)
+-- when opening a file, jump to the last position we were in that file (if there was one)
+autocmd(
+  group,
+  'BufReadPost',
+  "*",
+  function()
+    if vim.b.disable_jump_to_last_position then
+      return
+    end
+    if fn.line("'\"") > 0 and fn.line("'\"") <= fn.line("$") then
+      fn.execute('normal! g`\"')
+    end
+  end
+  ,
+  {
+    desc = 'Jump to last position when reopening a buffer'
+  }
+)
+
+-- vim.keymap.set('n', '<C-o>', function()
+--   local ts_utils = require'nvim-treesitter.ts_utils'
+--   local node = ts_utils.get_node_at_cursor(0, true)
+--   if not node then
+--     return
+--   end
+--   print(node:type())
+-- end)
 
 vim.cmd 'source ~/.config/nvim/startup.vim'
 
