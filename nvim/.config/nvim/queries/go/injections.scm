acd556803fa714ff1611fd46cfea1c646fd80cdc
Fred Dubois <169247+duboisf@users.noreply.github.com>
2022-06-23 12:55:32 -0400

    ✨ nvim: experiment with treesitter injections
    Created a go injection to inject graphql for string literals that have
    an variable name that contains 'graphql'

---
+++ nvim/.config/nvim/queries/go/injections.scm
@@ -0,0 +1,5 @@
+((identifier) @_graphql_ident (#vim-match? @_graphql_ident "\\cgraphql")
+    value: (expression_list
+        (raw_string_literal) @graphql))
+
+; ((identifier) @_graphql_ident (#vim-match? @_graphql_ident "\\cgraphql") value: (expression_list (raw_string_literal) @graphql))
