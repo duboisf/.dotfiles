ff8d4312b77e05f47da45119e3e4fcebdca86c44
Fred Dubois <169247+duboisf@users.noreply.github.com>
2022-06-26 10:37:47 -0400

    add nvim-tree config

--- nvim/.config/nvim/lua/cfg/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/cfg/plugins/nvim-tree.lua
@@ -1,1 +1,20 @@
-  require('nvim-tree').setup()
+require('nvim-tree').setup {
+  create_in_closed_folder = true,
+  hijack_cursor = true,
+  update_focused_file = {
+    enable = true,
+    update_root = true,
+  },
+  view = {
+    centralize_selection = true,
+    mappings = {
+      list = {
+        { key = 'u', action = 'dir_up' },
+      },
+    }
+  },
+}
+
+vim.cmd [[
+  nnoremap <leader>v :NvimTreeFindFileToggle<CR>
+]]
