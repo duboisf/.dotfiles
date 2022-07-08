f4770e4c96e3d51320bea6466c5cbee448dfae45
Fred Dubois <169247+duboisf@users.noreply.github.com>
2022-07-08 13:32:11 -0400

    add cwd to lualine

--- nvim/.config/nvim/lua/cfg/plugins/lualine.lua
+++ nvim/.config/nvim/lua/cfg/plugins/lualine.lua
@@ -1,3 +1,11 @@
+local utils = require('core.utils')
+
+local function get_cwd()
+  return ' ' .. utils.get_short_cwd()
+end
+
+-- to show current config, use:
+-- dump(require('lualine').get_config())
 require('lualine').setup {
   options = {
     theme = 'onedark',
@@ -6,12 +14,8 @@ require('lualine').setup {
     }
   },
   sections = {
-    lualine_c = {
-      {
-        'filename',
-        path = 1,
-      },
-    },
+    lualine_c = { { 'filename', path = 1 }, },
+    lualine_x = { get_cwd, "encoding", "fileformat", "filetype" },
     lualine_z = { 'location', '%L' },
   },
   tabline = {
