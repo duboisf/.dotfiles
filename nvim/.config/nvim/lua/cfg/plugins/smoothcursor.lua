49bd07235662b3c52d5c20d8aafabbe2a479f33e
Fred Dubois <169247+duboisf@users.noreply.github.com>
2022-09-20 09:40:38 -0400

    🎨 Change SmoothCursor symbols

--- nvim/.config/nvim/lua/cfg/plugins/smoothcursor.lua
+++ nvim/.config/nvim/lua/cfg/plugins/smoothcursor.lua
@@ -8,12 +8,12 @@ require('smoothcursor').setup {
         enable = true,       -- enable fancy mode
         head = { cursor = "▶", texthl = "SmoothCursor", linehl = nil },
         body = {
-            { cursor = "", texthl = "SmoothCursorRed" },
-            { cursor = "", texthl = "SmoothCursorOrange" },
-            { cursor = "●", texthl = "SmoothCursorYellow" },
-            { cursor = "●", texthl = "SmoothCursorGreen" },
-            { cursor = "•", texthl = "SmoothCursorAqua" },
-            { cursor = ".", texthl = "SmoothCursorBlue" },
+            { cursor = "■", texthl = "SmoothCursorRed" },
+            { cursor = "◼", texthl = "SmoothCursorOrange" },
+            { cursor = "◼", texthl = "SmoothCursorYellow" },
+            { cursor = "◼", texthl = "SmoothCursorGreen" },
+            { cursor = "▪", texthl = "SmoothCursorAqua" },
+            { cursor = "▪", texthl = "SmoothCursorBlue" },
             { cursor = ".", texthl = "SmoothCursorPurple" },
         },
         tail = { cursor = nil, texthl = "SmoothCursor" }
