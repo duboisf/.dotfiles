2e05eed2e5aacc0f6734f23a9060dedc64f8db47
Fred Dubois <169247+duboisf@users.noreply.github.com>
2022-10-03 10:13:49 -0400

    🐛 Fix gitsigns mappings
    Copy/pasted the example from the gitsigns README

--- nvim/.config/nvim/lua/cfg/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/cfg/plugins/gitsigns.lua
@@ -13,34 +13,41 @@ gitsigns.setup {
   },
   -- word_diff = true,
   on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
     local function map(mode, l, r, opts)
       opts = opts or {}
       opts.buffer = bufnr
       vim.keymap.set(mode, l, r, opts)
     end
 
-    local function nmap(l, r, opts) map('n', l, r, opts) end
-
     -- Navigation
-    nmap(']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
-    nmap('[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
 
-    local gs = gitsigns
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
 
     -- Actions
-    map({ 'n', 'v' }, '<leader>hs', gs.stage_hunk)
-    map({ 'n', 'v' }, '<leader>hr', gs.reset_hunk)
-    nmap('<leader>hS', gs.stage_buffer)
-    nmap('<leader>hu', gs.undo_stage_hunk)
-    nmap('<leader>hR', gs.reset_buffer)
-    nmap('<leader>hp', gs.preview_hunk)
-    nmap('<leader>hb', function() gs.blame_line { full = true } end)
-    nmap('<leader>htb', gs.toggle_current_line_blame)
-    nmap('<leader>hd', gs.diffthis)
-    nmap('<leader>hD', function() gs.diffthis('~') end)
-    nmap('<leader>htd', gs.toggle_deleted)
+    map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+    map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+    map('n', '<leader>hS', gs.stage_buffer)
+    map('n', '<leader>hu', gs.undo_stage_hunk)
+    map('n', '<leader>hR', gs.reset_buffer)
+    map('n', '<leader>hp', gs.preview_hunk)
+    map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+    map('n', '<leader>tb', gs.toggle_current_line_blame)
+    map('n', '<leader>hd', gs.diffthis)
+    map('n', '<leader>hD', function() gs.diffthis('~') end)
+    map('n', '<leader>td', gs.toggle_deleted)
 
     -- Text object
-    map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
   end
 }
