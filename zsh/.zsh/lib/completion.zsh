a65ad97bafc133b301e38545251d5596968351b0
Fred Dubois <169247+duboisf@users.noreply.github.com>
2023-02-14 22:07:50 -0500

    Add aws completion

--- zsh/.zsh/lib/completion.zsh
+++ zsh/.zsh/lib/completion.zsh
@@ -1,4 +1,9 @@
 
+if (( $+commands[aws] )); then
+    autoload -U +X bashcompinit && bashcompinit
+    complete -C =aws_completer aws
+fi
+
 if (( $+commands[terraform] )); then
     autoload -U +X bashcompinit && bashcompinit
     complete -o nospace -C =terraform terraform
