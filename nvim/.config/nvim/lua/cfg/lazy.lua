4edfb36d77ab17c3bcefec3c16c28f08a43b9699
Fred Dubois <169247+duboisf@users.noreply.github.com>
2023-03-20 11:54:38 -0400

    🔧 Don't monitor changes plugin configs

--- nvim/.config/nvim/lua/cfg/lazy.lua
+++ nvim/.config/nvim/lua/cfg/lazy.lua
@@ -11,4 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup('cfg.plugins.lazy')
+require("lazy").setup('cfg.plugins.lazy', {
+  change_detection = {
+    enabled = false,
+  }
+})
