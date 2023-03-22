27c03419cf0d0c44a0524fd747099a5a894ccd65
Fred Dubois <169247+duboisf@users.noreply.github.com>
2023-03-22 06:36:25 -0400

    ✨ Add more mini plugins

--- nvim/.config/nvim/lua/cfg/plugins/lazy/mini.lua
+++ nvim/.config/nvim/lua/cfg/plugins/lazy/mini.lua
@@ -1,9 +1,23 @@
 return {
   'echasnovski/mini.nvim',
   config = function()
-    require('mini.indentscope').setup()
-    require('mini.jump').setup()
-    require('mini.surround').setup()
+    local animate = require 'mini.animate'
+    require('mini.animate').setup({
+      cursor = {
+        timing = animate.gen_timing.quadratic({
+          easing = 'in-out',
+          duration = 50,
+          unit = 'total'
+        })
+      },
+      scroll = {
+        enable = false,
+      }
+    })
+    require('mini.comment').setup({})
+    require('mini.indentscope').setup({})
+    require('mini.jump').setup({})
+    require('mini.surround').setup({})
   end,
   version = false
 }
