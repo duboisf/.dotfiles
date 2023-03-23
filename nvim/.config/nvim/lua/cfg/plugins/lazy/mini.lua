d6dd8df1287b32e3ae1a1a994f83373eefc0c16e
Fred Dubois <169247+duboisf@users.noreply.github.com>
2023-03-23 11:25:09 -0400

    🔧 Add mini.splitjoin, change indentscope animation

--- nvim/.config/nvim/lua/cfg/plugins/lazy/mini.lua
+++ nvim/.config/nvim/lua/cfg/plugins/lazy/mini.lua
@@ -14,10 +14,21 @@ return {
         enable = false,
       }
     })
+
     require('mini.comment').setup({})
-    require('mini.indentscope').setup({})
+    local MiniIndentscope = require('mini.indentscope')
+    MiniIndentscope.setup({
+      draw = {
+        animation = MiniIndentscope.gen_animation.quadratic({
+          easing = 'in-out',
+          duration = 100,
+          unit = 'total'
+        })
+      }
+    })
     require('mini.jump').setup({})
     require('mini.pairs').setup({})
+    require('mini.splitjoin').setup({})
     require('mini.surround').setup({})
   end,
   version = false
