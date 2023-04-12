36619cf9779033f80068d48989b994d0a9aa0968
Fred Dubois <169247+duboisf@users.noreply.github.com>
2023-04-12 09:56:11 -0400

    🐛 Fix bad command-line window highlight
    Only happens with bluloco color scheme. Turns out the culprit was
    mini.animate. Just disabled min.animate to fix the issue.

--- nvim/.config/nvim/lua/cfg/plugins/lazy/mini.lua
+++ nvim/.config/nvim/lua/cfg/plugins/lazy/mini.lua
@@ -1,20 +1,6 @@
 return {
   'echasnovski/mini.nvim',
   config = function()
-    local animate = require 'mini.animate'
-    require('mini.animate').setup({
-      cursor = {
-        timing = animate.gen_timing.quadratic({
-          easing = 'in-out',
-          duration = 50,
-          unit = 'total'
-        })
-      },
-      scroll = {
-        enable = false,
-      }
-    })
-
     require('mini.comment').setup({})
     local MiniIndentscope = require('mini.indentscope')
     MiniIndentscope.setup({
