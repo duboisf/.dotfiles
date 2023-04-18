673c1cead0493f181e9d4dc5a9277140d4b23aaa
Fred Dubois <169247+duboisf@users.noreply.github.com>
2023-04-18 07:37:27 -0400

    🔧 Move telescope prompt to middle

--- nvim/.config/nvim/lua/cfg/plugins/lazy/telescope.lua
+++ nvim/.config/nvim/lua/cfg/plugins/lazy/telescope.lua
@@ -296,6 +296,7 @@ local function config()
         vertical = {
           preview_height = 0.25,
           preview_cutoff = 1,
+          prompt_position = 'top'
         }
       },
       border = {},
@@ -436,10 +437,10 @@ local function config()
         | `!^music` | inverse-prefix-exact-match | Items that do not start with `music` |
         | `!.mp3$`  | inverse-suffix-exact-match | Items that do not end with `.mp3`    |
         ]]
-        fuzzy = true,                   -- false will only do exact matching
+        fuzzy = true, -- false will only do exact matching
         override_generic_sorter = true, -- override the generic sorter
-        override_file_sorter = true,    -- override the file sorter
-        case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+        override_file_sorter = true, -- override the file sorter
+        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
         -- the default case_mode is "smart_case"
       },
       themes = {},
