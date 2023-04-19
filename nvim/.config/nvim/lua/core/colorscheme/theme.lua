5b5ea566cf8f568bba334593b94ccbc57f88d6b6
Fred Dubois <169247+duboisf@users.noreply.github.com>
2023-04-19 07:56:15 -0400

    🎨 Add highlighting for readonly semantic token

--- nvim/.config/nvim/lua/core/colorscheme/theme.lua
+++ nvim/.config/nvim/lua/core/colorscheme/theme.lua
@@ -43,5 +43,6 @@ return lush.extends({ theme }).with(function(injected_functions)
 
     -- Don't use comment semantic highlighting, it hides treesitter comment highlighting like TODO/FIXME
     sym('@lsp.type.comment') {},
+    sym('@lsp.mod.readonly') { theme.Constant },
   }
 end)
