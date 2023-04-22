06596e96842bb96fd653762826613c87284eb914
Fred Dubois <169247+duboisf@users.noreply.github.com>
2023-04-22 08:43:02 -0400

    refac: Add functions and doc strings

--- nvim/.config/nvim/lua/core/mermaid.lua
+++ nvim/.config/nvim/lua/core/mermaid.lua
@@ -1,44 +1,90 @@
-local parser = vim.treesitter.get_parser(12)
-local tree = parser:parse()[1]
+local M = {}
 
-local query = vim.treesitter.query.parse('markdown', [[
-(fenced_code_block
-  ((info_string (language) @_lang (#eq? @_lang "mermaid")))
-  (code_fence_content) @content)
-]])
-
-local mermaid_diagrams = {}
-
-for capture_id, node, _ in query:iter_captures(tree:root(), 12, 0, -1) do
-  if query.captures[capture_id] == 'content' then
-    table.insert(mermaid_diagrams, vim.treesitter.get_node_text(node, 12))
-  end
+-- Get the treesitter tree for the buffer bufnr.
+--- @param bufnr number: Buffer handle, defaults to current
+--- @return TSTree
+local function get_tree(bufnr)
+  local parser = vim.treesitter.get_parser(bufnr, 'markdown')
+  return parser:parse()[1]
 end
 
-local lines = {
-  '<!DOCTYPE html>',
-  '<html>',
-  '<head>',
-  '<script type="module">',
-  "import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';",
-  'mermaid.initialize({ startOnLoad: true });',
-  '</script>',
-  '</head>',
-  '<body>',
-}
-
-for _, diagram in ipairs(mermaid_diagrams) do
-  table.insert(lines, '<div class="mermaid">')
-  for _, s in ipairs(vim.split(diagram, "\n")) do
-    table.insert(lines, s)
-  end
-  table.insert(lines, '</div>')
+-- Get a treesitter query to find mermaid diagrams in markdown.
+--- @return Query
+local function get_query()
+  return vim.treesitter.query.parse('markdown', [[
+    (fenced_code_block
+      ((info_string (language) @lang (#eq? @lang "mermaid")))
+      (code_fence_content) @content)
+  ]])
 end
 
-table.insert(lines, '</body>')
-table.insert(lines, '</html>')
+-- Get the mermaid diagrams in the buffer bufnr.
+--- @param bufnr number: Buffer handle, defaults to current
+--- @return table<string>: List of mermaid diagrams
+local function find_mermaid_diagrams(bufnr)
+  local tree = get_tree(bufnr)
+  local query = get_query()
+  local mermaid_diagrams = {}
+  for capture_id, node, _ in query:iter_captures(tree:root(), bufnr, 0, -1) do
+    if query.captures[capture_id] == 'content' then
+      table.insert(mermaid_diagrams, vim.treesitter.get_node_text(node, bufnr))
+    end
+  end
+  return mermaid_diagrams
+end
 
-local html_bufid = vim.fn.bufadd('~/Downloads/mermaid.html')
-vim.fn.bufload(html_bufid)
+-- Create an html file with the mermaid diagrams.
+--- @param divs table<string>: List of mermaid diagram divs
+--- @return string: Html file
+local function create_html(divs)
+  local html_template = [[
+<!DOCTYPE html>
+<html>
+<head>
+<script type="module">
+  import mermaid from https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs;
+  mermaid.initialize({ startOnLoad: true });
+</script>
+</head>
+<body>
+%s
+</body>
+</html>
+]]
+  return string.format(html_template, table.concat(divs, '\n'))
+end
 
-vim.api.nvim_buf_set_lines(html_bufid, 0, -1, false, lines)
+-- Create an html div for each mermaid diagram.
+--- @param mermaid_diagrams table<string>: List of mermaid diagram divs
+local function create_divs(mermaid_diagrams)
+  local divs = {}
+  for _, diagram in ipairs(mermaid_diagrams) do
+    local div = string.format([[
+    <div class="mermaid">
+    %s
+    </div>
+    ]], diagram)
+    table.insert(divs, div)
+  end
+  return divs
+end
+
+-- Preview the mermaid diagrams in the current buffer. Creates a new html file.
+--- @param bufnr number: Buffer handle, defaults to current
+--- @return nil
+function M.preview(bufnr)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+  local mermaid_diagrams = find_mermaid_diagrams(bufnr)
+  local divs = create_divs(mermaid_diagrams)
+  local html = create_html(divs)
+  local lines = vim.split(html, '\n')
+  local html_filename = vim.fn.expand('/tmp/mermaid.html')
+  local html_bufid = vim.fn.bufadd(html_filename)
+  vim.fn.bufload(html_bufid)
+  vim.api.nvim_buf_set_lines(html_bufid, 0, -1, false, lines)
+  print('Mermaid preview: file://' .. html_filename)
+  -- save the html file
+  vim.api.nvim_buf_call(html_bufid, function() vim.api.nvim_cmd({ cmd = 'write' }, { output = false }) end)
+end
+
+return M
