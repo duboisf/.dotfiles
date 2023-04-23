516f1ae0d980480fdef433c1136903dd3afe8c38
Fred Dubois <169247+duboisf@users.noreply.github.com>
2023-04-23 14:20:33 -0400

    Rewrite mappings in lua

---
+++ nvim/.config/nvim/plugin/mappings.lua
@@ -0,0 +1,14 @@
+local set = vim.keymap.set
+
+-- Useful map to move over a parenthesis that was injected by the autopairs plugin
+set('i', '<C-f>', '<C-o>l')
+
+-- Alt-u changes the cwd of the current tab up one level (to the parent folder)
+set('n', '<A-u>', function() vim.cmd.tcd('..') end)
+
+-- Alt-i either opens a popup with the current TreeSitter node or the current
+-- highlight group under the cursor
+set('n', '<A-i>', require('core.utils').inspect_highlight, {
+  silent = true,
+  desc = 'Inspect highlight under cursor'
+})
--- nvim/.config/nvim/plugin/mappings.vim
+++
@@ -1,9 +0,0 @@
-" Useful map to move over a parenthesis that was injected by the autopairs plugin
-imap <C-f> <C-o>l
-
-" Alt-u changes the cwd of the current tab up one level (to the parent folder)
-nmap <A-u> :tcd ..<CR>
-
-" Alt-i either opens a popup with the current TreeSitter node or the current
-" highlight group under the cursor
-nnoremap <silent><A-i> :lua require('core.utils').inspect_highlight()<CR>
