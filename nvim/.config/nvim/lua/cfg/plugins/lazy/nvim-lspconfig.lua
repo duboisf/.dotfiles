d68d396f96003d0f1b85940941ceab2128492288
Fred Dubois <169247+duboisf@users.noreply.github.com>
2023-04-24 10:55:54 -0400

    refac: don't format buffer when saving

--- nvim/.config/nvim/lua/cfg/plugins/lazy/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/cfg/plugins/lazy/nvim-lspconfig.lua
@@ -98,15 +98,15 @@ local function config()
             end, { buffer = bufnr, desc = "[lsp] format" })
 
             -- format on save
-            vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              buffer = bufnr,
-              group = group,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = bufnr, async = false })
-              end,
-              desc = "[lsp] format on save",
-            })
+            -- vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+            -- vim.api.nvim_create_autocmd("BufWritePre", {
+            --   buffer = bufnr,
+            --   group = group,
+            --   callback = function()
+            --     vim.lsp.buf.format({ bufnr = bufnr, async = false })
+            --   end,
+            --   desc = "[lsp] format on save",
+            -- })
           end
 
           if client.supports_method("textDocument/rangeFormatting") then
