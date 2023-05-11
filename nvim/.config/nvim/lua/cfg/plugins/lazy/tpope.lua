cdbe9790f35ebd2092bc48a713276ee0ac95c8e7
Fred Dubois <169247+duboisf@users.noreply.github.com>
2023-05-11 09:49:14 -0400

    fix(nvim-plugins): don't make fugitive lazy load

--- nvim/.config/nvim/lua/cfg/plugins/lazy/tpope.lua
+++ nvim/.config/nvim/lua/cfg/plugins/lazy/tpope.lua
@@ -1,13 +1,6 @@
 return {
-  -- 'tpope/vim-commentary',
   'tpope/vim-eunuch',
-  {
-    'tpope/vim-fugitive',
-    cmd = 'G',
-    keys = {
-      { ',g', ':G<CR>', 'Open fugitive' }
-    },
-  },
+  'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
   'tpope/vim-repeat',
   'tpope/vim-unimpaired',
