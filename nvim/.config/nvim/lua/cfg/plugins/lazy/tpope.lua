425a92fe48713c060b90705529efab01261b9971
Fred Dubois <169247+duboisf@users.noreply.github.com>
2023-05-12 17:18:31 -0400

    fix(fugitive): add missing mapping

--- nvim/.config/nvim/lua/cfg/plugins/lazy/tpope.lua
+++ nvim/.config/nvim/lua/cfg/plugins/lazy/tpope.lua
@@ -1,6 +1,11 @@
 return {
   'tpope/vim-eunuch',
-  'tpope/vim-fugitive',
+  {
+    'tpope/vim-fugitive',
+    config = function()
+      vim.keymap.set('n', ',g', ':Git<CR>', { noremap = true, silent = true })
+    end
+  },
   'tpope/vim-rhubarb',
   'tpope/vim-repeat',
   'tpope/vim-unimpaired',
