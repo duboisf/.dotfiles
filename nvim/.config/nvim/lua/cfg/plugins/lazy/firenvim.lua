b86af3c159f43282aea7cff77bed8e83d63b8569
Fred Dubois <169247+duboisf@users.noreply.github.com>
2023-09-21 13:25:08 -0400

    fix(firenvim): run firenvim#install function on update

--- nvim/.config/nvim/lua/cfg/plugins/lazy/firenvim.lua
+++ nvim/.config/nvim/lua/cfg/plugins/lazy/firenvim.lua
@@ -1,5 +1,10 @@
 return {
   'glacambre/firenvim',
+  cond = not not vim.g.started_by_firenvim,
+  build = function()
+    require("lazy").load({ plugins = { "firenvim" }, wait = true })
+    vim.fn["firenvim#install"](0)
+  end,
   config = function()
     local group = vim.api.nvim_create_augroup("cfg#firenvim", { clear = true })
     local firenvim = require('cfg.firenvim')
