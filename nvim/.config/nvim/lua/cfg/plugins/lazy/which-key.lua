7b37d52f2499fd1d4a2ea5ac6d2f92ef298f69af
Fred Dubois <169247+duboisf@users.noreply.github.com>
2024-09-25 08:20:43 -0400

    feat: add hydra-mode window actions to which-key

--- nvim/.config/nvim/lua/cfg/plugins/lazy/which-key.lua
+++ nvim/.config/nvim/lua/cfg/plugins/lazy/which-key.lua
@@ -1,5 +1,11 @@
 return {
   "folke/which-key.nvim",
+  config = function()
+    local wk = require("which-key")
+    wk.add({
+      { "<C-w><space>", function() wk.show({ keys = "<C-w>", loop = true, desc = "Hydra-mode window actions" }) end },
+    })
+  end,
   event = "VeryLazy",
   opts = {
     preset = "helix",
