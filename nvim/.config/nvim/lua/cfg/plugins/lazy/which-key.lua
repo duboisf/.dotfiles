83e93f64daa3c49d7493b20bd5b31052c9ea1fde
Fred Dubois <169247+duboisf@users.noreply.github.com>
2024-09-26 10:04:05 -0400

    fix(nvim): load which-key config correctly

--- nvim/.config/nvim/lua/cfg/plugins/lazy/which-key.lua
+++ nvim/.config/nvim/lua/cfg/plugins/lazy/which-key.lua
@@ -1,11 +1,5 @@
 return {
   "folke/which-key.nvim",
-  config = function()
-    local wk = require("which-key")
-    wk.add({
-      { "<C-w><space>", function() wk.show({ keys = "<C-w>", loop = true, desc = "Hydra-mode window actions" }) end },
-    })
-  end,
   opts = {
     preset = "helix",
   },
