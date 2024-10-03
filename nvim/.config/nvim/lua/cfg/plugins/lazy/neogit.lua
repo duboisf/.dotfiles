44abbd39591ab43b532b9ccaa4481ece8da8f4c4
Fred Dubois <169247+duboisf@users.noreply.github.com>
2024-10-03 16:25:34 -0400

    feat(neogit): add keymap for checking out recent branch

--- nvim/.config/nvim/lua/cfg/plugins/lazy/neogit.lua
+++ nvim/.config/nvim/lua/cfg/plugins/lazy/neogit.lua
@@ -5,10 +5,14 @@ return {
     "sindrets/diffview.nvim", -- optional - Diff integration
     "nvim-telescope/telescope.nvim",
   },
-  config = {
-    kind = 'auto',
-    integrations = {
-      telescope = false,
-    },
-  }
+  config = function()
+    local neogit = require('neogit')
+    neogit.setup({
+      kind = 'auto',
+      integrations = {
+        telescope = false,
+      },
+    })
+    vim.keymap.set('n', '<A-s>', neogit.action('branch', 'checkout_recent_branch'))
+  end
 }
