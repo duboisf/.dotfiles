0b5f35bf969f0d0e07f2c147848c8a511ce0cb3b
Fred Dubois <169247+duboisf@users.noreply.github.com>
2024-10-03 13:51:19 -0400

    feat(nvim): add CopilotChat plugin

---
+++ nvim/.config/nvim/lua/cfg/plugins/lazy/copilotchat.lua
@@ -0,0 +1,10 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  branch = "canary",
+  dependencies = {
+    { "zbirenbaum/copilot.lua" },
+    { "nvim-lua/plenary.nvim" },
+  },
+  build = "make tiktoken",
+  opts = true,
+}
