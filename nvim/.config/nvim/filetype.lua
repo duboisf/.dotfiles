9323903237b655314892eac3934f5934e0529f2c
Fred Dubois <169247+duboisf@users.noreply.github.com>
2025-03-03 09:10:22 -0500

    fix(nvim): remove no longer needed terraform filetype assignment

--- nvim/.config/nvim/filetype.lua
+++ nvim/.config/nvim/filetype.lua
@@ -1,11 +1,4 @@
 vim.filetype.add({
-  extension = {
-    -- Theres a bug currently where tfvars files get assigned the
-    -- terraform-vars filetype and you get a bunch of errors like this:
-    --   E5248: Invalid character in group name
-    -- The "fix" is to just set the filetype to terraform.
-    tfvars = "terraform"
-  },
   pattern = {
     -- when editing the current command line in vim from zsh by doing ctrl-x,
     -- ctrl-e, set the filetype as bash. This is just to get treesitter to
