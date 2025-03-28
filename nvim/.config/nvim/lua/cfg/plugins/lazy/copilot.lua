9067bf748275664aef08830d959beeb27dc1eb5c
Fred Dubois <169247+duboisf@users.noreply.github.com>
2025-03-28 14:10:15 -0400

    fix(copilot): enable auto trigger

--- nvim/.config/nvim/lua/cfg/plugins/lazy/copilot.lua
+++ nvim/.config/nvim/lua/cfg/plugins/lazy/copilot.lua
@@ -1,7 +1,6 @@
 return {
   "zbirenbaum/copilot.lua",
   cmd = 'Copilot',
-  dependencies = { 'folke/which-key.nvim' },
   cond = require('core.utils').has_network,
   event = 'InsertEnter',
   config = function()
@@ -19,7 +18,7 @@ return {
       },
       suggestion = {
         enabled = true,
-        auto_trigger = false,
+        auto_trigger = true,
         keymap = {
           accept = false,
           accept_word = '<M-l>',
@@ -27,24 +26,18 @@ return {
       }
     }
 
-    local function toggle_auto_trigger()
-      require('copilot.suggestion').toggle_auto_trigger()
-    end
-
-    -- add keymap to toggle auto trigger <leader>cc (code complete)
-    require('which-key').add({
-      {
-        '<leader>cc',
-        function()
-          require('copilot.suggestion').toggle_auto_trigger()
-          if vim.b.copilot_suggestion_auto_trigger then
-            print('AI code suggestion auto trigger is enabled')
-          else
-            print('AI code suggestion auto trigger is disabled')
-          end
-        end,
-        desc = 'Toggle AI suggestion auto trigger',
-      },
-    })
+    vim.keymap.set(
+      'n',
+      '<leader>ct',
+      function()
+        require('copilot.suggestion').toggle_auto_trigger()
+        if vim.b.copilot_suggestion_auto_trigger then
+          print('Copilot auto trigger is enabled')
+        else
+          print('Copilot auto trigger is disabled')
+        end
+      end,
+      { desc = 'Toggle Copilot LSP' }
+    )
   end,
 }
