56aa5d94c74745cb211f91e1c83e6566595d298c
Fred Dubois <169247+duboisf@users.noreply.github.com>
2025-04-07 10:42:57 -0400

    fix(which-key): worakaround for <C-o> in insert mode

--- nvim/.config/nvim/lua/cfg/plugins/lazy/which-key.lua
+++ nvim/.config/nvim/lua/cfg/plugins/lazy/which-key.lua
@@ -1,7 +1,18 @@
 return {
   "folke/which-key.nvim",
+  enabled = true,
   opts = {
     preset = "helix",
     sort = { "alphanum", "local", "order", "group", "mod" },
   },
+  config = function(_, opts)
+    require('which-key').setup(opts)
+
+    -- vim.keymap.set('i', , rhs, opts?)
+    -- worakaround for <C-o>, see https://github.com/folke/which-key.nvim/issues/827
+    vim.keymap.set("i", "<C-o>", function()
+      vim.cmd("startinsert!")
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, true, true), 'n', false)
+    end, { expr = true })
+  end
 }
