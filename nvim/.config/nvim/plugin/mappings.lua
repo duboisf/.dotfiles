8439f2ce99f075b8ebbbf8f701b21f476fac7c15
Fred Dubois <169247+duboisf@users.noreply.github.com>
2025-04-30 17:51:58 -0400

    feat(nvim): add mapping to increment character under cursor

--- nvim/.config/nvim/plugin/mappings.lua
+++ nvim/.config/nvim/plugin/mappings.lua
@@ -33,3 +33,21 @@ set(
     silent = true,
   }
 )
+
+set(
+  'n',
+  '<C-u>',
+  function()
+    local _, col = unpack(vim.api.nvim_win_get_cursor(0))
+    local line = vim.api.nvim_get_current_line()
+    local char = vim.fn.strcharpart(line, col, 1)
+    local code = vim.fn.char2nr(char)
+    local next_char = vim.fn.nr2char(code + 1)
+    print('char', char, 'code', code, 'next_char', next_char)
+    vim.cmd([[normal! r]] .. next_char)
+  end,
+  {
+    desc = "Increment character under cursor, supports unicode",
+    silent = true,
+  }
+)
