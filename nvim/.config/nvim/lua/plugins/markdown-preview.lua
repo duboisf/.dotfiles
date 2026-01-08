40e6b3424a2e3f414cdc49074afdc1464a3987f1
Fred Dubois <169247+duboisf@users.noreply.github.com>
2026-01-08 09:22:19 -0500

    fix: don't echo the markdown-preview url

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -4,7 +4,6 @@ return {
   ft = { 'markdown' },
   config = function()
     vim.g.mkdp_auto_close = 0
-    vim.g.mkdp_browserfunc = 'EchoMarkdownPreviewUrl'
     vim.g.mkdp_echo_preview_url = 1
     vim.g.mkdp_filetypes = { "markdown" }
   end,
--- nvim/.config/nvim/plugin/markdown-preview.vim
+++
@@ -1,3 +0,0 @@
-function EchoMarkdownPreviewUrl(url)
-  " noop
-endfunction
