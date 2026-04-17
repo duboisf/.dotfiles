2496a44b8ba4d5d7b13a34f29ac0dadd59bba210
Fred Dubois <169247+duboisf@users.noreply.github.com>
2026-04-17 09:40:36 -0400

    fix(nvim): promote textobjects to a separate plugin

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,9 +4,6 @@ return {
     'nvim-treesitter/nvim-treesitter',
     enabled = true,
     build = ':TSUpdate',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
     ---@module 'nvim-treesitter.configs'
     ---@type TSConfig
     opts = {
@@ -60,61 +57,78 @@ return {
         enable = true,
         disable = { 'yaml' }
       },
-      textobjects = {
-        -- syntax-aware textobjects
-        select = {
-          enable = true,
-          lookahead = true,
-          disable = {},
-          keymaps = {
-            -- custom captures
-            ["al"] = "@list.outer",
-            ["il"] = "@list.inner",
-            ["ak"] = "@key.outer",
-            ["ik"] = "@key.inner",
-
-            -- the queries are in textobjects.scm
-            ["aa"] = "@assignment.outer",
-            ["ia"] = "@assignment.inner",
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ap"] = "@parameter.outer",
-            ["ip"] = "@parameter.inner",
-            ["aC"] = "@class.outer",
-            ["iC"] = "@class.inner",
-            ["ac"] = "@conditional.outer",
-            ["ic"] = "@conditional.inner",
-            ["ab"] = "@block.outer",
-            ["ib"] = "@block.inner",
-            ["aL"] = "@loop.outer",
-            ["iL"] = "@loop.inner",
-            ["is"] = "@statement.inner",
-            ["as"] = "@statement.outer",
-            ["ad"] = "@comment.outer",
-            ["am"] = "@call.outer",
-            ["im"] = "@call.inner"
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            [']F'] = '@function.outer',
-            ['<C-j>'] = '@parameter.inner'
-          },
-          goto_previous_start = {
-            ['[F'] = '@function.outer',
-            ['<C-k>'] = '@parameter.inner'
-          },
-          goto_next_end = {},
-          goto_previous_end = {},
-        },
-        swap = {
-          enable = true,
-        },
-      }
     },
     main = 'nvim-treesitter.configs',
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    branch = 'main',
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-treesitter-textobjects').setup {
+        select = {
+          lookahead = true,
+        },
+        move = {
+          set_jumps = true,
+        },
+      }
+
+      local select = require('nvim-treesitter-textobjects.select')
+      local move = require('nvim-treesitter-textobjects.move')
+
+      local select_keymaps = {
+        -- custom captures
+        al = '@list.outer',
+        il = '@list.inner',
+        ak = '@key.outer',
+        ik = '@key.inner',
+
+        -- the queries are in textobjects.scm
+        aa = '@assignment.outer',
+        ia = '@assignment.inner',
+        af = '@function.outer',
+        ['if'] = '@function.inner',
+        ap = '@parameter.outer',
+        ip = '@parameter.inner',
+        aC = '@class.outer',
+        iC = '@class.inner',
+        ac = '@conditional.outer',
+        ic = '@conditional.inner',
+        ab = '@block.outer',
+        ib = '@block.inner',
+        aL = '@loop.outer',
+        iL = '@loop.inner',
+        ['is'] = '@statement.inner',
+        as = '@statement.outer',
+        ad = '@comment.outer',
+        am = '@call.outer',
+        im = '@call.inner',
+      }
+      for lhs, capture in pairs(select_keymaps) do
+        vim.keymap.set({ 'x', 'o' }, lhs, function()
+          select.select_textobject(capture, 'textobjects')
+        end)
+      end
+
+      local move_keymaps = {
+        goto_next_start = {
+          [']F'] = '@function.outer',
+          ['<C-j>'] = '@parameter.inner',
+        },
+        goto_previous_start = {
+          ['[F'] = '@function.outer',
+          ['<C-k>'] = '@parameter.inner',
+        },
+      }
+      for fn, maps in pairs(move_keymaps) do
+        for lhs, capture in pairs(maps) do
+          vim.keymap.set({ 'n', 'x', 'o' }, lhs, function()
+            move[fn](capture, 'textobjects')
+          end)
+        end
+      end
+    end,
   },
   {
     'nvim-treesitter/nvim-treesitter-context',
